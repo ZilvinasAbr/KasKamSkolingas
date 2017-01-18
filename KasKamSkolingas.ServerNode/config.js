e88372d9a7bcf57bcad598e833cf6023cee1bfd4
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-01-18 11:59:59 +0200

    Configured mongodb, implemented a prototype to register a user.

--- KasKamSkolingas.ServerNode/config.js
+++ KasKamSkolingas.ServerNode/config.js
@@ -2,5 +2,6 @@ module.exports = {
   jwtSecret: 'MyS3cr3tK3Y',
   jwtSession: {
     session: false
-  }
+  },
+  dbConnectionUrl: 'mongodb://localhost:27017/kaskamskolingas'
 };
--- KasKamSkolingas.ServerNode/routes/account.js
+++ KasKamSkolingas.ServerNode/routes/account.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const bcrypt = require('bcrypt-nodejs');
+
+const { dbConnectionUrl } = require('../config');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Account index');
+router.post('/register', (req, res) => {
+  MongoClient.connect(dbConnectionUrl, (err, db) => {
+    assert.equal(null, err);
+
+    const { userName, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      res.send(false);
+      db.close();
+      return;
+    }
+
+    const encryptedPassword = bcrypt.hashSync(password);
+
+    db.collection('users').insertOne({
+      userName,
+      password: encryptedPassword
+    })
+    .then((result) => {
+      console.log('Inserted user into users collection');
+      db.close();
+      res.send(true);
+    })
+    .catch(err2 => console.error(err2));
+  });
 });
 
 module.exports = router;
